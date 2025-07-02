72651db921b15d1a832dbccf8ff44375cf1c752b
lehoangphuc747 <lphc664@yahoo.com.vn>
2025-07-02 19:05:05 +0700

    Setup GitHub Pages deployment with GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lehoangphuc747.github.io',
+  base: '/hocanki',
   integrations: [react(), mdx()],
 
   vite: {
